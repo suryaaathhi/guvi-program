84624dcda9d95bfe189a6f9c1169af4f78bc2ef5
suryaaathhi <45627768+suryaaathhi@users.noreply.github.com>
2019-07-19 11:21:47 +0530

    Update maxarr.py

--- maxarr.py
+++ maxarr.py
@@ -1,7 +1,6 @@
-n=int(input())
-a=[]
-for i in range(1,n+1):
-    x=int(input())
-    a.append(x)
-print(max(a))
-    
+list1 = [] 
+num = int(input()) 
+for i in range(1, num + 1): 
+    ele = int(input()) 
+    list1.append(ele)
+print(max(list1)) 
