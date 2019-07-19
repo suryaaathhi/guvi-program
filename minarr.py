569a1d402bdef35e84888eb5355506ab1ce6240f
suryaaathhi <45627768+suryaaathhi@users.noreply.github.com>
2019-07-19 11:36:03 +0530

    Update minarr.py

--- minarr.py
+++ minarr.py
@@ -1,6 +1,4 @@
-list1 = [] 
-num = int(input()) 
-for i in range(1, num + 1): 
-    ele = int(input()) 
-    list1.append(ele)
-print(min(list1)) 
+ac=int(input())
+bc=list(map(int,input().split()))
+if (len(bc)==ac):
+    print(min(bc))
